e4d0cd19f7c86deadfe5ae8f232abc56d1848fc7
Harish772 <40069272+Harish772@users.noreply.github.com>
2022-01-23 10:12:10 -0500

    Add files via upload

---
+++ Queries/Import-CmModule.ps1
@@ -0,0 +1,9 @@
+﻿[CmdletBinding()]
+Param()
+
+if ( -not (Test-Path -Path "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManager.psd1") ) {
+    Write-Error -Message 'ConfigurationManager module does not exist.'
+    exit
+}
+
+Import-Module -FullyQualifiedName "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManager.psd1"
---
+++ Queries/Update-DPClientCertificate.ps1
@@ -0,0 +1,30 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$CAName,
+    [Parameter(Mandatory = $true)]
+    [string]$TemplateName
+)
+
+# Import CM module and change to SMS drive path
+Import-Module -FullyQualifiedName "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManager.psd1"
+$SiteCode = (Get-CimInstance -Namespace ROOT/SMS -ClassName SMS_ProviderLocation).SiteCode
+Set-Location -Path "$($SiteCode):"
+
+# Request and export cert
+$Timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+$SubjectName = "CN=ConfigMgr Client $Timestamp"
+$Certificate = Get-Certificate -Url "LDAP:////$($CAName)" -Template $TemplateName -SubjectName $SubjectName -CertStoreLocation 'Cert:\LocalMachine\My\'
+$Thumbprint = $Certificate.Certificate.Thumbprint
+$Password = (New-Guid).Guid | ConvertTo-SecureString -AsPlainText -Force
+Export-PfxCertificate -Cert "Cert:\LocalMachine\My\$Thumbprint" -FilePath "C:\$Thumbprint.pfx" -Password $Password | Out-Null
+
+# Loop through DPs and update cert
+$DPs = ((Get-CMDistributionPoint -SiteCode $SiteCode).NetworkOSPath).trim('\\')
+foreach ($DP in $DPs) {
+    Set-CMDistributionPoint -SiteSystemServerName $DP -CertificatePath "C:\$Thumbprint.pfx" -CertificatePassword $Password -Confirm:$false -Force
+}
+
+# Remove local copies of cert
+Remove-Item -Path "Cert:\LocalMachine\My\$Thumbprint" -Force
+Remove-Item -Path "C:\$Thumbprint.pfx" -Force
